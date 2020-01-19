0232cf896f2f0baba1e644ac2bd3e32ca8ed3141
Stuart Fox <stuart@stuartfox.ca>
2020-01-19 00:57:56 -0800

    EOD

--- demo/app/routes.py
+++ demo/app/routes.py
@@ -1,12 +1,13 @@
-from flask import render_template, Response
 from app import app
+from flask import render_template, Response, request
+from datetime import datetime
 
+import hashlib
+import json
+import mysql.connector as mysql
 import os
+import pickle
 import redis
-
-def __init__():
-    r = redis_connect()
-    return r
 
 
 @app.after_request
@@ -21,20 +22,70 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/api/v1.0/elasticache/flush')
+@app.route('/api/v1.0/elasticache/flush', methods=['GET'])
 def elasticache_flush():
     r = redis_connect()
+    start = datetime.now()
     r.flushall()
-    return Response("{'Response': 'Elasticache successfully flushed'}", mimetype='application/json')
+    stop = datetime.now()
+    diff = (stop-start).total_seconds()
+    payload = json.dumps({"Response": "Elasticache successfully flushed",
+                          "Duration": str(diff) + " seconds"})
+    return Response(payload,
+                    mimetype='application/json')
 
+
+@app.route('/api/v1.0/elasticache/query', methods=['POST'])
+def elasticache_query():
+    r = redis_connect()
+    content = request.get_json(silent=True)
+    q = content['Query'].encode('utf-8')
+    hex_dig = hashlib.sha256(q).hexdigest()
+
+    if r.exists(hex_dig):
+        start = datetime.now()
+        data = r.get(hex_dig)
+        stop = datetime.now()
+        diff = (stop-start).total_seconds()
+        payload = json.dumps({"Response": "CACHE HIT",
+                          "Duration": str(diff) + " seconds"})
+        return Response(payload, mimetype='application/json')
+    else:
+        db = db_connect()
+        cursor = db.cursor()
+        query = q
+        start = datetime.now()
+        cursor.execute(query)
+        records = cursor.fetchall()
+        r.set(hex_dig, pickle.dumps(records))
+        stop = datetime.now()
+        diff = (stop-start).total_seconds()
+        payload = json.dumps({"Response": "CACHE MISS",
+                          "Duration": str(diff) + " seconds"})
+        return Response(payload, mimetype='application/json')
 
 @app.route('/api/v1.0/dax')
 def dax():
-    return Response("DAX", mimetype='application/json')
+    payload = json.dumps({"Response": "DAX"})
+    return Response(payload, mimetype='application/json')
 
 
 def redis_connect():
-    r = redis.Redis(host=os.environ['CACHE_HOST'],
-                    port=6379,
-                    db=0)
-    return r
+    try:
+        r = redis.Redis(host=os.environ['CACHE_HOST'],
+                        port=6379,
+                        db=0)
+        return r
+    except redis.RedisError as e:
+        print(e)
+        payload=json.dumps({"Response": "Error Connecting to os.environ['CACHE_HOST']"})
+        return Response(payload, mimetype='application/json')
+
+            
+def db_connect():
+    db = mysql.connect(host      = os.environ['DB_HOST'],
+                        user     = os.environ['DB_USER'],
+                        passwd   = os.environ['DB_PASS'],
+                        database = 'employees')
+
+    return db
--- demo/app/static/aws-caching.js
+++ demo/app/static/aws-caching.js
@@ -1,16 +1,32 @@
+var apiBase = "http://"+location.hostname+"/api/v1.0"
 
-function triggerAWS() {
-    
+function flushCache() { 
     const xhr = new XMLHttpRequest(),
         method = "GET",
-        url="http://"+location.hostname+"/api/v1.0/elasticache/flush";
+        url=apiBase + "/elasticache/flush";
 
     xhr.open(method, url, true);
     xhr.onreadystatechange = function () {
         if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            document.getElementById(responseID).innerHTML = xhr.responseText;
+            document.getElementById("responseID").innerHTML = xhr.responseText;
         }
     };
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send();
+}
+
+function runQuery() {
+    const xhr = new XMLHttpRequest(),
+        method = "POST",
+        url=apiBase + "/elasticache/query";
+
+    xhr.open(method, url, true);
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            document.getElementById("responseID").innerHTML = xhr.responseText;
+        }
+    };
+    xhr.setRequestHeader("Content-Type", "application/json");
+    qstr = document.getElementById("queryString").value
+    xhr.send(JSON.stringify({"Query": qstr}));
 }
