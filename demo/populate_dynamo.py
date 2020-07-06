6ac077012fd20f42539fd70a1a9eb71ea58f73e6
Stuart Fox <stuart@stuartfox.ca>
2020-07-05 21:28:32 -0700

    Add multiprocessing to populate_dynamo to speed up data creation

--- demo/populate_dynamo.py
+++ demo/populate_dynamo.py
@@ -18,7 +18,6 @@ WESTERNMOST  = -124.
 SENSORS      = 500
 RECORDS      = 10000
 T            = SENSORS*RECORDS
-WORKERS      = 10
 
 # For testing against local
 if 'DYNAMODB_ENDPOINT' in os.environ:
@@ -43,21 +42,8 @@ def _gen_lat_lng():
     return(lat,lng)
 
 
-def populate(start_point,stamps):
-    return
-
-print('Checking DynamoDB')
-if 'Item' not in item:
-    print('Populating DynamoDB Table with ' + str(T), ' records, this could take a while')
-
-    dt = datetime.datetime(2016, 2, 25, 23, 23)
-    startTime=(round(time.mktime(dt.timetuple())))
-    stamps = [startTime]
-    for record in range(0, RECORDS):
-        startTime += 10
-        stamps.append(startTime)
-
-    for i in range(1, SENSORS):
+def populate(start, end, stamps):
+    for i in range(start, end):
         sensor = 'sensor' + str(i)
         sTime = startTime
         print(sensor)
@@ -73,5 +59,29 @@ if 'Item' not in item:
             for stamp in stamps:
                 item = {'sensorName': sensor, 'timestamp': stamp, 'datapoint': random.randint(20,90)}
                 batchData.put_item(Item=item)
+
+def start_workers(stamps):
+    jobs = []
+    for number in range(0, 500, 50):
+        args = {"start":number,"end":(number+49), "stamps":stamps}
+        p = multiprocessing.Process(target=populate, kwargs=args)
+        jobs.append(p)
+        p.start()
+
+
+
+print('Checking DynamoDB')
+if 'Item' not in item:
+    print('Populating DynamoDB Table with ' + str(T), ' records, this could take a while')
+
+    dt = datetime.datetime(2016, 2, 25, 23, 23)
+    startTime=(round(time.mktime(dt.timetuple())))
+    stamps = [startTime]
+    for record in range(0, RECORDS):
+        startTime += 10
+        stamps.append(startTime)
+
+    start_workers(stamps)
+
 else:
     print("DynamoDB Table Already Populated with test data")
